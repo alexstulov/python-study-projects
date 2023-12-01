77d440d763f4f0d40c2321d46d55319d81e92120
Alex Stulov <a.stulov.web@gmail.com>
2023-12-01 13:29:22 +0300

    Initialize time calculator

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 ## Use `$ python3 src/main.py` to run all projects samples and tests
 
 ## FCC basic course
-Here are several projects from [Scientific Computing with Python](https://www.freecodecamp.org/learn/scientific-computing-with-python/scientific-computing-with-python-projects/time-calculator).
+Here are several projects from [Scientific Computing with Python](https://www.freecodecamp.org/learn/scientific-computing-with-python/scientific-computing-with-python-projects/).
--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 # This entrypoint file to be used in development. Start by reading README.md
-from pytest import main
+from pytest import main as pytest_main
+from unittest import main as unittest_main
 
 from arithmetic_arranger.arithmetic_arranger import arithmetic_arranger
-
+from time_calculator.time_calculator import add_time
 # arithmetic_arranger example code
 print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
+# time_calculator example code
+print(add_time("11:06 PM", "2:02"))
 
 # Run unit tests automatically
-main(['-vv'])
+pytest_main(['-vv'])
+unittest_main(module='tests.test_time_calculator', exit=False)
