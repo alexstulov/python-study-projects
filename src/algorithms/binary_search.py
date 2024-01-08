d63e64ac89fb5ab2852d20cd634869943a89a305
Alex Stulov <a.stulov.web@gmail.com>
2024-01-08 17:46:50 +0300

    Improve binary search algorithm

--- src/algorithms/binary_search.py
+++ src/algorithms/binary_search.py
@@ -1,17 +1,15 @@
 def binary_search(list, item):
-    low = 0
-    high = len(list) - 1
-    while low < high:
-        mid = round((high - low) / 2)
-        guess = list[mid]
-        if guess == item:
-            return mid
-        elif guess > item:
-            high = mid - 1
-        elif guess < item:
-            low = mid + 1
-    return None
-    
-# my_list = [1,3,5,7,9]
-# print(binary_search(my_list, 3), 1)
-# print(binary_search(my_list, -1), 'None')
+    left, right = 0, len(list)-1
+    while left <= right:
+        middle = (left+right) // 2
+        if item == list[middle]:
+            return middle
+        elif item < list[middle]:
+            right = middle - 1
+        else:
+            left = middle + 1
+    return -1
+my_list = [1,3,5,7,9]
+print(binary_search(my_list, 3), 1)
+print(binary_search(my_list, -1), -1)
+print(binary_search([2,7,8,9,10], 9), 3)
