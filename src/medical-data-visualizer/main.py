264fe1a934e99bab36fe38b70deabad3929b5b9f
Alex Stulov <a.stulov.web@gmail.com>
2024-03-13 10:24:14 +0300

    Add medical data visualizer project

---
+++ src/medical-data-visualizer/main.py
@@ -0,0 +1,11 @@
+# Project from https://www.freecodecamp.org/learn/data-analysis-with-python/data-analysis-with-python-projects/demographic-data-analyzer
+# To finish put files and commit at https://freecodecam-boilerplate-ekqcnrvhu92.ws-us108.gitpod.io/
+import medical_data_visualizer
+from unittest import main
+
+# Test your function by calling it here
+medical_data_visualizer.draw_cat_plot()
+medical_data_visualizer.draw_heat_map()
+
+# Run unit tests automatically
+main(module='test_module', exit=False)
