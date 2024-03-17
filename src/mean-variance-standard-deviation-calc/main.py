eaf4d00e0805a654af4dd6c0803972258f06ff2c
Alex Stulov <a.stulov.web@gmail.com>
2024-03-17 11:11:00 +0300

    Move MVSD project to separate repo: https://github.com/alexstulov/mean-variance-std

--- src/mean-variance-standard-deviation-calc/main.py
+++
@@ -1,4 +0,0 @@
-# Solve DA problem from FCC course: 
-# https://www.freecodecamp.org/learn/data-analysis-with-python/data-analysis-with-python-projects/mean-variance-standard-deviation-calculator
-import mean_var_std
-import test_module
